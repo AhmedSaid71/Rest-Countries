d39922aa9a129f5a763693eec666b1f7f280e57a
LordAhmed71 <ahmed55mohamed88@gmail.com>
2024-06-13 13:57:34 +0300

    feat RCS-19 create go back button

--- src/pages/Country.tsx
+++ src/pages/Country.tsx
@@ -1,10 +1,23 @@
 import { useCountry } from "../hooks";
 import { Loader } from "../components";
+import { Button } from "antd";
+import { GoArrowLeft } from "react-icons/go";
+import { useNavigate } from "react-router-dom";
 const Country = () => {
   const { country, isPending } = useCountry();
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1);
+  };
   if (isPending) return <Loader />;
   return (
     <section className="flex gap-8 flex-col mt-8">
+      <div>
+        <Button onClick={handleGoBack} className="px-8 shadow-md py-4">
+          <GoArrowLeft />
+          Back
+        </Button>
+      </div>
       <div className="flex gap-8">
         <div className="flex-1">
           <img
